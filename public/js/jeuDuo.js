835324be6ec23a8f1de65901c422e5b3862b08be
ghysal <jonathan.ritter94@gmail.com>
2018-02-12 14:53:14 +0100

    test js damier

---
+++ public/js/jeuDuo.js
@@ -0,0 +1,31 @@
+var posRed = [];
+var posBlue = [];
+
+alert("test");
+
+jQuery(function() {
+  function range(n) {
+    return [...Array(n).keys()]
+  }
+
+  function getDamier(n) {
+    return range(n).map(function(_, i) {
+      return range(n).map(function(_, j) {
+        return (i + j) % 2;
+      });
+    });
+  }
+
+  function printDamier(n) {
+    var damier = jQuery('#damier');
+    getDamier(n).forEach(function(l, i) {
+      var line = jQuery('<tr></tr>').appendTo(damier);
+      l.forEach(function(c, j) {
+        var color = c ? 'white' : 'black';
+        line.append('<td class="' + color + '"></td>');
+      });
+    });
+  }
+
+  printDamier(10);
+})
